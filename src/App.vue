8e56815bedacd5399f242a66933012585f1e51ff
rjdeleon03 <ricardopablo.deleon@gmail.com>
2019-06-29 19:50:46 +0800

    Add new project files

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<style>
-body {
-  background-image: linear-gradient(#0277bd, #4fc3f7);
-  background-attachment: fixed;
-}
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
 #app {
-  font-family: "Nunito Sans", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  font-family: "Nunito Sans", Helvetica, Arial, sans-serif;
-}
-
-.mdl-button {
-  font-family: "Nunito Sans", Helvetica, Arial, sans-serif;
-  font-weight: 600;
 }
 </style>
