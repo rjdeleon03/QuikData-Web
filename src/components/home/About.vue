402ddd0a71f4d8c06639cdfecc012052840f7e8d
rjdeleon03 <ricardopablo.deleon@gmail.com>
2019-07-18 22:53:26 +0800

    Add icons

--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -6,8 +6,7 @@
       <p>Ilocos Center for Research, Empowerment, and Development (ICRED), together with the community of Vintar in Ilocos Norte, worked on the two components of this innovative solution.</p>
       <hr />
       <h2 class="center">Consortium</h2>
-      <p>Quik Data has two components - (1) Community-Based Disaster Management and (2) the Quik Data mobile app. The CBDM will train community members on disaster risk management and help them create their own Community Disaster Preparedness Plans. This will ensure that they are capacitated to clearly and quickly communicate what support they need when a disaster strikes. The Quik Data app will serve as the reporting tool during calamities where the community can send their situation so the response will be appropriate to their needs. The app has been tested by our community partner and their feedback was crucial in the development of its design.</p>
-      <p>Ilocos Center for Research, Empowerment, and Development (ICRED), together with the community of Vintar in Ilocos Norte, worked on the two components of this innovative solution.</p>
+      <p>Lorem ipsum dolor selet sit amet.</p>
     </div>
   </div>
 </template>
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -16,8 +16,9 @@
           <div class="col s12 m12 l4 xl4" id="text">
             <div id="main-text-wrapper">
               <!-- <h2>Quik Data</h2> -->
+              <img src="@/assets/logo.png" style="margin-top: 20px; margin-bottom: -10px;" />
               <p>A Community-Based Disaster Reporting App by Ilocos Center for Research, Empowerment and Development (ICRED)</p>
-              <a href="#" class="btn-flat amber darken-2">Know More</a>
+              <a v-scroll-to="'#about'" class="btn-flat amber darken-2">Know More</a>
             </div>
           </div>
         </div>
@@ -71,6 +72,10 @@ export default {
 .home #main-text-wrapper p {
   font-size: 1.5em;
   font-weight: 600;
+}
+.home #main-text-wrapper img {
+  width: 50%;
+  max-width: 200px;
 }
 @media only screen and (min-width: 373px) {
   .home #video-container .col#video {
